1b36da8a1c21695b43e6e6b57f5222a0e34db19c
FatemehHabibzadeh <49490306+FatemehHabibzadeh@users.noreply.github.com>
2019-05-08 11:51:19 +0430

    queez

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getMaxListeners } from "cluster";
+import { throwStatement } from "@babel/types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: ''};
+    this.state = {family-name: ''};
+    this.state = {id: ''};
+    this.state = {email: ''};
+  }
+
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.id);
+    event.preventDefault();
+
+    alert('A name was submitted: ' + this.state.name);
+    event.preventDefault();
+
+    alert('A name was submitted: ' + this.state.family-name);
+    event.preventDefault();
+
+    alert('A name was submitted: ' + this.state.email);
+    event.preventDefault();
+
+
+  }
+     person1{
+      this.setState({
+        [id]:1
+        [name]: fatemeh
+        [family-name]:habibzadeh
+        [email]:fatemeh.habibzadeh@gmail.com
+      });
+  
+    document.write(`${person1}<br/>`)
+
+    person2{
+      this.setstate({
+        [id]:2
+        [name]:zahra
+        [family-name]:khoshkho
+        [email]:zahra.khoshkho@gmail.com
+      });
+      document.write(`${person2}<br/>`)
+    }
+
+    person3{
+      this.setstate({
+        [id]:3
+        [name]:fatemeh
+        [family-name]:abbasi
+        [email]:zahra.abbasi@gmail.com
+      });
+      document.write(`${person3}<br/>`)
+    }
+   
+
+ 
+ 
+
+
+
+  render() 
+
+
+return (
+ 
+
 
 export default App;
